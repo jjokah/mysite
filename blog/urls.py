b6c36614051a9f1a84320e81d69a4a70e8f8e4a6
jay <johnjohnsonokah@gmail.com>
2020-04-14 17:45:19 +0100

    Add templates, pagination and class-based view

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,8 @@ app_name = 'blog'
 
 urlpatterns = [
     # post views
-    path('', views.post_list, name='post_list'),
+    # path('', views.post_list, name='post_list'),  # fuction based view
+    path('', views.PostListView.as_view(), name='post_list'),  # class view
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_detail,
          name='post_detail',),
